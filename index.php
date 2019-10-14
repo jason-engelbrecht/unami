455c39924f6075120940f72f0bbe941a5b6e847c
Maxwell Lee <maxwelllee12345@yahoo.com>
2019-10-14 18:58:15 -0400

    Made form1 use POST and the index page puts the info into the hive

--- index.php
+++ index.php
@@ -35,13 +35,80 @@ if(!isset($_SESSION['user']))
 $f3->route('GET /', function()
 {
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/form1.html');
 });
 
-$f3->route('GET /review', function()
+$f3->route('GET|POST /personal_information', function($f3)
+{
+    if(!empty($_POST))
+    {
+        // get data from form
+        $first = $_POST['first'];
+        $last = $_POST['last'];
+        $pronouns = $_POST['pronouns'];
+        $address = $_POST['inputAddress'];
+        $address2 = $_POST['inputAddress2'];
+        $city = $_POST['inputCity'];
+        $state = $_POST['inputState'];
+        $zip = $_POST['inputZip'];
+        $primaryPhone = $_POST['primary'];
+        $primaryTime = $_POST['primary_time'];
+        $alternatePhone = $_POST['alternate'];
+        $alternateTime = $_POST['alternate_time'];
+        $emergency_name = $_POST['emergency_name'];
+        $emergency_email = $_POST['emergency_email'];
+
+        // add data to hive
+        $f3->set('fname', $first);
+        $f3->set('lname', $last);
+        $f3->set('pronouns', $pronouns);
+        $f3->set('address', $address);
+        $f3->set('address2', $address2);
+        $f3->set('city', $city);
+        $f3->set('state', $state);
+        $f3->set('zip', $zip);
+        $f3->set('primary_phone', $primaryPhone);
+        $f3->set('primary_time', $primaryTime);
+        $f3->set('alternate_phone', $alternatePhone);
+        $f3->set('alternate_time', $alternateTime);
+        $f3->set('emergency_name', $emergency_name);
+        $f3->set('emergency_email', $emergency_email);
+
+        // validate data
+
+        $f3->reroute('/additional_information');
+    }
+
+   $view = new Template();
+   echo $view->render('views/form1.html');
+});
+
+$f3->route('GET|POST /additional_information', function($f3)
+{
+    if(!empty($_POST))
+    {
+        // get data from form
+
+        // add data to hive
+
+        // validate data
+
+        $f3->reroute('/review');
+    }
+
+    $view = new Template();
+    echo $view->render('views/form2.html');
+});
+
+$f3->route('GET|POST /review', function()
 {
     $view = new Template();
     echo $view->render('views/review.html');
+});
+
+$f3->route('GET|POST /performance_agreement', function() {
+    $view = new Template();
+    echo $view->render('views/performanceAgreement.html');
 });
 
 //Run fat-free
