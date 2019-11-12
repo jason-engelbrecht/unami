fede607fcb3db68b0249ce64b42cecfedbc0ed30
alvinnava13 <anava8@mail.greenriver.edu>
2019-11-12 17:19:03 -0500

    Reformat P2P and ETS training descriptions and performance agreements and create test routes for them

--- index.php
+++ index.php
@@ -684,5 +684,33 @@ $f3->route('GET /unit_testing', function() {
     echo $view->render('model/unit_testing/validationTesting.php');
 });
 
+// test route for ETS training description
+$f3->route('GET /test_ETStrainingDescription', function() {
+    $view = new Template();
+
+    echo $view->render('views/forms/specific_form_pages/ETS/ETStrainingDescription.html');
+});
+
+// test route for ETS performance agreement
+$f3->route('GET /test_ETSperformanceAgreement', function() {
+    $view = new Template();
+
+    echo $view->render('views/forms/specific_form_pages/ETS/performanceAgreement.html');
+});
+
+// test route for P2P training description
+$f3->route('GET /test_P2PtrainingDescription', function() {
+    $view = new Template();
+
+    echo $view->render('views/forms/specific_form_pages/P2P/P2PtrainingDescription.html');
+});
+
+// test route for ETS performance agreement
+$f3->route('GET /test_P2PperformanceAgreement', function() {
+    $view = new Template();
+
+    echo $view->render('views/forms/specific_form_pages/P2P/performanceAgreement.html');
+});
+
 //Run fat-free
 $f3->run();
