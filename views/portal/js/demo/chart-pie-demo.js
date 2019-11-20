807367174f1f0ea8ba8e1cf3b6628583d25ae32a
Jason Engelbrecht <jengelbrecht2@mail.greenriver.edu>
2019-11-20 18:21:47 -0500

    Start pie chart

--- views/portal/js/demo/chart-pie-demo.js
+++ views/portal/js/demo/chart-pie-demo.js
@@ -2,12 +2,13 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
+
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Submitted", "Referral", "Social"],
     datasets: [{
       data: [55, 30, 15],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
