146dae25de493245ee61838f1989b8ea986e054f
Jason Engelbrecht <jengelbrecht2@mail.greenriver.edu>
2019-12-12 02:33:12 -0500

    Clean up

---
+++ model/dummy_data/app_type_info.sql
@@ -0,0 +1,15 @@
+-- active
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (1, '10/01/2019', 'Krajan', '08/08/2019', 1, '05/22/2019', 1);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (2, '09/17/2019', 'Provo', '04/21/2019', 2, '02/02/2019', 1);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (3, '01/10/2019', 'Baitao', '09/11/2019', 3, '04/15/2019', 1);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (4, '04/22/2019', 'Denpasar', '05/13/2019', 1, '02/06/2019', 1);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (5, '03/21/2019', 'Gorzyce', '03/26/2019', 2, '08/31/2019', 1);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (6, '08/25/2019', 'Carabamba', '12/31/2018', 3, '04/29/2019', 1);
+
+-- inactive
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (7, '10/09/2019', 'Valuyki', '09/24/2019', 1, '03/10/2019', 0);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (8, '02/13/2019', 'Burunday', '07/30/2019', 1, '03/11/2019', 0);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (9, '07/24/2019', 'Ntungamo', '03/17/2019', 2, '07/08/2019', 0);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (10, '06/08/2019', 'Sunduan', '04/15/2019', 2, '06/02/2019', 0);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (11, '02/08/2019', 'Valle Grande', '07/17/2019', 2, '02/07/2019', 0);
+insert into app_type_info (info_id, date, location, deadline, app_type, date2, active) values (12, '01/25/2019', 'Banqiao', '05/04/2019', 3, '03/15/2019', 0);
